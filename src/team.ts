ac22fd0ea122688a18480e47eba95e2f61f20a6b
Daniel Motz <hello@danielmotz.de>
2025-09-01 08:31:03 +0000

    Add team section with member data and components

---
+++ src/team.ts
@@ -0,0 +1,185 @@
+// Team data for the team section
+export const teamMembers = [
+  // MEPHisto, Friedrich-Schiller-Universität Jena
+  {
+    name: "Prof. Dr. Clemens Beckstein",
+    title: "Prof. Dr.",
+    position: "Projektleiter",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Apl. Prof. Dr. Robert Gramsch-Stehfest",
+    title: "Apl. Prof. Dr.",
+    position: "Teilprojektleiter",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Clemens Beck, M.A.",
+    title: "M.A.",
+    position: "Projektkoordinator",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Johannes Mitschunas, M.Sc.",
+    title: "M.Sc.",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Nico Faltin, M.A.",
+    title: "M.A.",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Rebecca Pongratz, B.Sc.",
+    title: "B.Sc.",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Daniel Motz, B.Sc.",
+    title: "B.Sc.",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Samuel Glowka, B.A.",
+    title: "B.A.",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Patrick Stahl",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Jessi Ann Moßler",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Lotta Frey",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Felix Staacke",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Tobias Eckardt",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+  {
+    name: "Luca Nauschütz",
+    title: "",
+    position: "",
+    group: "MEPHisto, Friedrich-Schiller-Universität Jena",
+    publications: []
+  },
+
+  // Arbeitsstelle Germania Sacra
+  {
+    name: "Bärbel Kröger, M.A.",
+    title: "M.A.",
+    position: "Teilprojektleiterin",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Dr. Christian Popp",
+    title: "Dr.",
+    position: "Teilprojektleiter",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Dr. Martin Kuric",
+    title: "Dr.",
+    position: "",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Luana Moraes Costa",
+    title: "",
+    position: "",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Daniel Lukas Osman",
+    title: "",
+    position: "",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Felix Schwart",
+    title: "",
+    position: "",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+  {
+    name: "Smitesh Sorathiya",
+    title: "",
+    position: "",
+    group: "Arbeitsstelle Germania Sacra, Niedersächsische Akademie der Wissenschaften",
+    publications: []
+  },
+
+  // Deutsches Historisches Institut Rom
+  {
+    name: "Dr. Jörg Hörnschemeyer",
+    title: "Dr.",
+    position: "Teilprojektleiter",
+    group: "Deutsches Historisches Institut Rom",
+    publications: []
+  },
+  {
+    name: "Maximilian Berens, M.A.",
+    title: "M.A.",
+    position: "",
+    group: "Deutsches Historisches Institut Rom",
+    publications: []
+  },
+
+  // FactGrid
+  {
+    name: "Dr. Olaf Simons",
+    title: "Dr.",
+    position: "Teilprojektleiter",
+    group: "FactGrid",
+    publications: []
+  },
+  {
+    name: "Tinghui Duan",
+    title: "",
+    position: "",
+    group: "FactGrid",
+    publications: []
+  },
+];
